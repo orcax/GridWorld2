ce275405fbab00dfd867caf295293908b7543599
orcax <siriusxyk@gmail.com>
2015-10-11 17:38:52 -0400

    finish part 0

--- src/main/java/edu/rudcs/gridworld/test/MapTester.java
+++ src/main/java/edu/rudcs/gridworld/test/MapTester.java
@@ -7,11 +7,11 @@ public class MapTester {
 
     public static void main(String[] args) {
         Generator gen = null;
-        gen = new MapGenerator(101, 101);
+        gen = new MapGenerator(51, 51);
         // gen = new MazeGenerator(50, 100, 0.5, 0.5);
 
-        // gen = new DfsMazeGenerator(25, 25);
-        gen.generateMaps(1000);
+//        gen = new DfsMazeGenerator(50, 50);
+        gen.generateMaps(1);
 
     }
 
