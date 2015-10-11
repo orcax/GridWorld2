ed0038e2aa103c6ff65b81dbedf26078f79d1b64
orcax <siriusxyk@gmail.com>
2015-10-10 23:31:37 -0400

    fix map generator

--- src/main/java/edu/rudcs/gridworld/test/MapTester.java
+++ src/main/java/edu/rudcs/gridworld/test/MapTester.java
@@ -1,15 +1,15 @@
 package edu.rudcs.gridworld.test;
 
-import edu.rudcs.gridworld.map.DfsMazeGenerator;
 import edu.rudcs.gridworld.map.Generator;
+import edu.rudcs.gridworld.map.MapGenerator;
 
 public class MapTester {
 
     public static void main(String[] args) {
         Generator gen = null;
-        // gen = new MapGenerator();
+        gen = new MapGenerator(101, 101);
         // gen = new MazeGenerator(50, 100, 0.5, 0.5);
-        gen = new DfsMazeGenerator(25, 25);
+        // gen = new DfsMazeGenerator(25, 25);
         gen.generateMaps(1);
     }
 
