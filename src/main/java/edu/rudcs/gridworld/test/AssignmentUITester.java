53ed033d24740b90a2d1bfa22d550b03d2bf0183
orcax <siriusxyk@gmail.com>
2015-10-14 20:50:54 -0400

    final update

--- src/main/java/edu/rudcs/gridworld/test/AssignmentUITester.java
+++ src/main/java/edu/rudcs/gridworld/test/AssignmentUITester.java
@@ -1,37 +1,23 @@
 package edu.rudcs.gridworld.test;
 
-import edu.rudcs.gridworld.agent.AdaptiveAstarAgent;
-import edu.rudcs.gridworld.agent.Agent;
-import edu.rudcs.gridworld.agent.RawAdaptiveAgent;
-import edu.rudcs.gridworld.agent.RawRepeatBackwardAgent;
-import edu.rudcs.gridworld.agent.RawRepeatForwardAgent;
-import edu.rudcs.gridworld.agent.RepeatBackwardAstarAgent;
-import edu.rudcs.gridworld.agent.RepeatForwardAstarAgent;
-import edu.rudcs.gridworld.map.RepeatAstarSearchMap;
 
 public class AssignmentUITester {
 	
-	public static final String map1 = "maps/map7.txt";
+	public static final String map1 = "maps/map1.txt";
 	public static final String map2 = "maps/randmazes/1.txt";
 	public static final String map3 = "maps/randmaps/1.txt";
 	
 	public static void main(String[] args){
-		//AssignmentUITester at = new AssignmentUITester();
-	
 		run(map1);
 		run(map2);
 		run(map3);
-		
-		//at.runRepeatAstarAgent();
 	}
 	
 	private static void run(String map){
-		RunRepeatAstarAgent.runRepeatAstarAgent("forward", map);
-	    RunRepeatAstarAgent.runRepeatAstarAgent("backward", map);
 		RunRepeatAstarAgent.runRepeatAstarAgent("adaptive", map);
+	    RunRepeatAstarAgent.runRepeatAstarAgent("backward", map);
+		RunRepeatAstarAgent.runRepeatAstarAgent("forward", map);
 		RunRepeatAstarAgent.runRepeatAstarAgent("Rawforward", map);
-		//RunRepeatAstarAgent.runRepeatAstarAgent("Rawbackward", map);
-		//RunRepeatAstarAgent.runRepeatAstarAgent("Rawadaptive", map);
 	}
 
 }
--- src/main/java/edu/rudcs/gridworld/test/MapTester.java
+++ src/main/java/edu/rudcs/gridworld/test/MapTester.java
@@ -1,5 +1,6 @@
 package edu.rudcs.gridworld.test;
 
+import edu.rudcs.gridworld.map.DfsMazeGenerator;
 import edu.rudcs.gridworld.map.Generator;
 import edu.rudcs.gridworld.map.MapGenerator;
 
@@ -7,12 +8,9 @@ public class MapTester {
 
     public static void main(String[] args) {
         Generator gen = null;
-        gen = new MapGenerator(51, 51);
-        // gen = new MazeGenerator(50, 100, 0.5, 0.5);
-
-//        gen = new DfsMazeGenerator(50, 50);
-        gen.generateMaps(1);
-
+        gen = new MapGenerator(101, 101);
+        gen.generateMaps(50);
+        gen = new DfsMazeGenerator(101, 101);
+        gen.generateMaps(50);
     }
-
 }
